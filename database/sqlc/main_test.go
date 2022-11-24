254c54b4a4a74bf50a0e895fc96bb9191f114852
MehrshadDarvish <MehrshadDarvish>
2022-11-24 17:50:54 +0330

    [ add ] store and store test for automic transaction

--- database/sqlc/main_test.go
+++ database/sqlc/main_test.go
@@ -9,7 +9,8 @@ import (
 	_ "github.com/lib/pq"
 )
 
-var testQueries * Queries
+var testQueries *Queries
+var testDB *sql.DB
 
 const (
 	dbDriver = "postgres"
@@ -17,11 +18,13 @@ const (
 )
 
 func TestMain(m *testing.M) {
-	conn, err := sql.Open( dbDriver, dbSource )
+	var err error
+
+	testDB, err = sql.Open(dbDriver, dbSource)
 	if err != nil {
-		log.Fatal( "cannot connect to database:", err)
+		log.Fatal("cannot connect to database:", err)
 	}
 
-	testQueries = New(conn)
-	os.Exit( m.Run() )
+	testQueries = New(testDB)
+	os.Exit(m.Run())
 }
